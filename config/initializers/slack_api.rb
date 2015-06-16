436d37a792e508bea40dc5f6ef99a927ee76fa1d
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-06-15 22:26:26 -0500

    Use the RTM API to listen for new users and welcome them

--- config/initializers/slack_api.rb
+++ config/initializers/slack_api.rb
@@ -3,3 +3,13 @@ require 'slack'
 Slack.configure do |config|
   config.token = ENV['slack_api_token']
 end
+
+client = Slack.realtime
+
+client.on :team_join do |data|
+  welcome = "Welcome to slashrocket, <@#{data['user']}>! Type `/rocket` for a quick tour of our channels. :simple_smile:"
+  options = { channel: '#general', text: welcome, as_user: true }
+  Slack.chat_postMessage(options)
+end
+
+client.start
