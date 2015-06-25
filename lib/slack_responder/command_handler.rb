0856cb59fcfdf9944a64f5b6b0d9d4e015c06ce4
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-06-25 12:19:08 -0500

    Update formatting

--- lib/slack_responder/command_handler.rb
+++ lib/slack_responder/command_handler.rb
@@ -25,19 +25,22 @@ module SlackResponder
     private
 
     def tour
-      '*Welcome to **slashrocket**!*
+      '*Welcome to slashrocket!*
       *Here\'s a quick channel breakdown:*
+
       *#general* - everything tech/programming related :computer:
       *#random* - for anything that\'s not :simple_smile:
-      *#debug* - you need help with your code, this is the place to ask :bug:
+      *#debug* - if you need help with your code, this is the place to ask :bug:
       *#newbies* - feel free to take it slow, we\'re happy to help :smile_cat:
-      *#team_projects* - help us build something or start a project of your own. Our GitHub repo is at github.com/slashrocket :white_check_mark:
+      *#team_projects* - help us build something or start a project of your own - check out <https://github.com/slashrocket|our GitHub> :white_check_mark:
       *#resources* - relevant links, videos, etc that could help the learning process :book:
       *#hackathon* - discuss a past, current, or upcoming hackathon :rocket:
-      *#meta* - all things slashrocket. Whether it\'s good, bad, or anything in between, the admin team is listening. :cop:
+      *#meta* - all things slashrocket. Whether it\'s good, bad, or anything in between, the admin team is listening :cop:
       *#gaming* - fun chat about all platforms, all games :video_game:
+
       We also have a <https://community.slashrocket.io|great forum> open to anyone on slashrocket! :book:
-      *Message @seanosaur, @4xposed, @mike, @paulstraw, or @mcmahoniel if you need to get in touch with an admin.* :ghost:'
+
+      *Message @seanosaur, @4xposed, @mike, @paulstraw, or @mcmahoniel if you need to get in touch with an admin* :ghost:'
     end
 
     # Check if this is a slash command or a mention.
