de4b2b7575e21c51a49591891379935dc769b664
Daniel James McMahon <danieljmcmahon@gmail.com>
2016-03-26 23:02:08 -0700

    Log to console but suppress live messages on user joins

--- config/initializers/slack_api.rb
+++ config/initializers/slack_api.rb
@@ -8,10 +8,10 @@ client = Slack::RealTime::Client.new
 
 client.on :team_join do |data|
   puts "New User, #{data['user']['name']}, Joined"
-  welcome = "Welcome to slashrocket, <@#{data['user']['id']}>! Type `/rocket` for a quick tour of our channels. :simple_smile:"
-  options = { channel: '#general', text: welcome, as_user: true }
-  Slack.chat_postMessage(options)
-  puts "Welcomed #{data['user']['name']}"
+  # welcome = "Welcome to slashrocket, <@#{data['user']['id']}>! Type `/rocket` for a quick tour of our channels. :simple_smile:"
+  # options = { channel: '#general', text: welcome, as_user: true }
+  # Slack.chat_postMessage(options)
+  # puts "Welcomed #{data['user']['name']}"
 end
 
 Thread.new do
